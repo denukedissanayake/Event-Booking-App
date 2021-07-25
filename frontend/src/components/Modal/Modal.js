cb47b053fe73b4a3d4a6769c0f99652efa97d33a
Denuke <ddnadhananja@gmail.com>
2021-07-26 02:09:09 +0530

    Add Modal

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 import './Modal.css'
 
-const modal = props => {
+const modal = props => (
     <div className="modal">
-        <header>{props.header}</header>
+        <header className="modal__header"><h1>{props.title}</h1></header>
         <section className="modal__content">
             {props.children}
         </section>
         <section className="modal__actions">
-            {props.canCancel && <button className="btn">Cancel</button>}
-            {props.canConfirm && <button className="btn">Confirm</button>}
+            {props.canCancel && <button className="btn" onClick={props.onCancel}>Cancel</button>}
+            {props.canConfirm && <button className="btn" onClick={props.onConfirm}>Confirm</button>}
         </section>
     </div>
-};
+);
 
 export default modal;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
 
-import '../index.css'
-import './Events.css'
+import '../index.css';
+import './Events.css';
 
 class EventsPage extends Component{
+
+    state = {
+        creating : false
+    }
+
+    startCreateEventHandler = () => {
+        this.setState({creating:true})
+    }
+
+    modalConfirmHandler = () => {
+        this.setState({creating:false})
+    }
+
+    modalCencelHandler = () => {
+        this.setState({creating:false})
+    }
+
     render() {
-        return <div className="events-control">
-            <h3>Let's Share your Own Event!</h3>
-            <button className="btn">
-              Create Event
-            </button>
-        </div>
+        return (
+            <React.Fragment>
+                {this.state.creating && <Backdrop />}
+                {this.state.creating &&  <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCencelHandler} onConfirm={this.modalConfirmHandler}>
+                    <p>Modal</p>
+                </Modal>}
+                <div className="events-control">
+                    <h3>Let's Share your Own Event!</h3>
+                    <button className="btn" onClick={this.startCreateEventHandler}>
+                        Create Event
+                    </button>
+                </div>
+            </React.Fragment>
+        );
     }
 }
 
